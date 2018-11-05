8c465f7b12d2e4085b42c241e3495cc6e50e12bb
otakupasi <otakupasi@gmail.com>
2018-11-05 18:13:46 +0000

    minor code changes

---
+++ src/main/IOHandler.java
@@ -0,0 +1,14 @@
+package main;
+
+public class IOHandler {
+    public double startTime;
+
+    public void start() {
+        this.startTime = System.currentTimeMillis();
+    }
+    //TODO: importGraphFrom(String path):Graph
+    //TODO: importQueryFrom(String path)
+    //TODO: assertTimeout: Graphimport under 2 minutes
+    //TODO: exportSolutionTo(String path, solution):void
+    //TODO: diff(String pathSol, String pathSolution):bool
+}
