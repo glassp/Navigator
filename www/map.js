7edecf6a24a408af726c8e63680f48a5eefb7306
sphynxsilver <schroth.stephan@web.de>
2019-02-14 15:57:42 +0100

    implement initMap()
    using Open Street Map tiles

--- www/map.js
+++ www/map.js
@@ -4,3 +4,30 @@
 //TODO: handleClick(e) -> Snackbar("You clicked at e.lat, e.long")
 //TODO: addEventlistner DOMContentLoader->init, click->handleClick
 //TODO: function getRoute(startLat, startLong, destLat, destLong) and helper functions
+
+
+// Init map
+
+// uing default lat/lon for Uni Stuttgart, IT building
+// 48.7451 9.1067
+
+
+function initMap() {
+    document.getElementById('helperLine').innerHTML = 'Initializing map now.';    
+    var mapVar = L.map('map', {
+        center: [48.7451, 9.1067],
+        zoom: 15
+    });
+    
+    // .setView([48.7451, 9.1067], 12) alternatively, as used in quick start guide
+
+    
+    // using Open Street Map tiles.
+    L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        subdomains: ['a', 'b', 'c']
+    }).addTo( mapVar );
+
+    document.getElementById('helperLine').innerHTML='Map initialization successfull.';
+
+} 
